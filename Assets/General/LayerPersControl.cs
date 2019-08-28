96486fa0073574877b3c2f976d191483d677e9fd
chepays <52157587+chepays@users.noreply.github.com>
2019-08-28 23:04:24 +0500

    11

--- Assets/General/LayerPersControl.cs
+++ Assets/General/LayerPersControl.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Anima2D;
 
 public class LayerPersControl : MonoBehaviour
 {
     public GameObject _torso;//Торс героя
+    public GameObject _torso2;//Одежда
+    public GameObject _head;//голова
     public GameObject _lArm;//левая рука
+    public GameObject _lArm2;//Левая Кисть
     public GameObject _rArm;//правая рука
+    public GameObject _rArm2;//правая кисть
     public GameObject _lLeg;//левая нога
+    public GameObject _lBoots;//Левый сапог
     public GameObject _rLeg;//правая нога
+    public GameObject _rBoots;//Правый сапог
     public GameObject _lArmpit;//левая подмышка
     public GameObject _rArmpit;//правая подмышка
     public GameObject _shadow;//Тень
@@ -39,12 +46,18 @@ public class LayerPersControl : MonoBehaviour
     {
         int LeftRite = 1;
         SetLayerQ(_torso, LayerAll);
-        SetLayerQ(_lArm, LayerAll + 2 * LeftRite);
-        SetLayerQ(_rArm, LayerAll - 2 * LeftRite);
-        SetLayerQ(_lLeg, LayerAll + 1 * LeftRite);
-        SetLayerQ(_rLeg, LayerAll - 1 * LeftRite);
-        SetLayerQ(_rArmpit, LayerAll - 1 * LeftRite);
-        SetLayerQ(_lArmpit, LayerAll + 1 * LeftRite);
+        SetLayerQ(_head, LayerAll);
+        SetLayerQ(_torso2, LayerAll + 1 * LeftRite);
+        SetLayerQ(_lArm, LayerAll + 3 * LeftRite);
+        SetLayerQ(_rArm, LayerAll - 3 * LeftRite);
+        SetLayerQ(_lArm2, LayerAll + 3 * LeftRite);
+        SetLayerQ(_rArm2, LayerAll - 3 * LeftRite);
+        SetLayerQ(_lLeg, LayerAll + 2 * LeftRite);
+        SetLayerQ(_rLeg, LayerAll - 2 * LeftRite);
+        SetLayerQ(_lBoots, LayerAll + 2 * LeftRite);
+        SetLayerQ(_rBoots, LayerAll - 2 * LeftRite);
+        SetLayerQ(_rArmpit, LayerAll - 2 * LeftRite);
+        SetLayerQ(_lArmpit, LayerAll + 2 * LeftRite);
         SetLayerQ(_shadow, LayerAll);
         PeS.LayerPers = LayerAll;
     }
@@ -52,6 +65,10 @@ public class LayerPersControl : MonoBehaviour
     {
         if (A != null)
         {
+            if (A.GetComponent<SpriteMeshInstance>())
+            {
+                A.GetComponent<SpriteMeshInstance>().sortingOrder = int.Parse(C.ToString());
+            }
             if (A.GetComponent<SpriteRenderer>())
             {
                 A.GetComponent<SpriteRenderer>().sortingOrder = int.Parse(C.ToString());
